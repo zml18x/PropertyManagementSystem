97fc2dac8cd79e24ede31b0ae1dda2128a2f5383
ZML <zml18v@gmail.com>
2023-04-11 22:31:11 +0200

    Added database using EF

---
+++ src/PMS.Infrastructure/Container/InfrastructureDependencies.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PMS.Infrastructure.Data.Context;
+
+namespace PMS.Infrastructure.Container
+{
+    public static class InfrastructureDependencies
+    {
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<PmsDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
---
+++ src/PMS.Infrastructure/Data/Context/PmsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace PMS.Infrastructure.Data.Context
+{
+    public class PmsDbContext : DbContext
+    {
+        public PmsDbContext(DbContextOptions<PmsDbContext> options) : base(options)
+        {
+
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
