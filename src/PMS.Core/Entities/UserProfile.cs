844c5f1cc5603d8bc9e4cfb357ec994970692146
ZML <zml18v@gmail.com>
2023-04-16 20:52:48 +0200

    Added to UserProfile model validation for Names and phoneNumber

--- src/PMS.Core/Entities/UserProfile.cs
+++ src/PMS.Core/Entities/UserProfile.cs
@@ -1,4 +1,6 @@
-﻿namespace PMS.Core.Entities
+﻿using System.Text.RegularExpressions;
+
+namespace PMS.Core.Entities
 {
     public class UserProfile : Entity
     {
@@ -21,10 +23,16 @@
 
         private void SetNames(string firstName, string lastName)
         {
-            // VALIDATION
+            if (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName) || firstName == string.Empty || lastName == string.Empty)
+                throw new ArgumentNullException("The names fields cannot be empty");
 
-            if(firstName == null || lastName == null)
-                throw new ArgumentNullException(nameof(firstName));
+            if (firstName.Length < 2 || lastName.Length < 2)
+                throw new ArgumentException("First name & last name must be at least 2 characters long");
+
+            Regex regex = new Regex("^[a-zA-Z]+$");
+
+            if (!regex.IsMatch(firstName) || !regex.IsMatch(lastName))
+                throw new ArgumentException("First name or last name contains prohibited characters");
 
             FirstName = firstName;
             LastName = lastName;
@@ -32,10 +40,18 @@
 
         private void SetPhoneNumber(string phoneNumber)
         {
-            // VALIDATION
+            // Validation for the Polish number standard
 
-            if(phoneNumber == null)
-                throw new ArgumentNullException(nameof(phoneNumber));
+            if (string.IsNullOrWhiteSpace(phoneNumber) || string.IsNullOrEmpty(phoneNumber))
+                throw new ArgumentNullException(nameof(phoneNumber),"Number phone cannot be null or white space");
+
+            if (phoneNumber.Length != 9)
+                throw new ArgumentException("The number should consist of 9 digits",nameof(phoneNumber));
+
+            Regex regex = new Regex("^[0-9]+$");
+
+            if (!regex.IsMatch(phoneNumber))
+                throw new ArgumentException("The number can only consist of digits", nameof(phoneNumber));
 
             PhoneNumber = phoneNumber;
         }
